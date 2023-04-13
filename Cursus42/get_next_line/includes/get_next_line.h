e07225058818a7d967af3fec4ab4cbbfccf8559d
Matias Dreesen <mdreesen@a1-r1-p2.s19.be>
2023-04-13 15:01:02 +0200

    get_next_line finished

--- Cursus42/get_next_line/includes/get_next_line.h
+++ Cursus42/get_next_line/includes/get_next_line.h
@@ -11,13 +11,13 @@
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-char *get_next_line(int fd);
-char *ft_strjoin(char *s1, char *s2);
-char *ft_strchr(char *str, char c);
-char *ft_strdup(char *s1);
-char *ft_strncat(char *dest, char *src, int n);
-int ft_strlen(const char *str);
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *str, char c);
+char	*ft_strdup(char *s1);
+char	*ft_strncat(char *dest, char *src, int n);
+int		ft_strlen(const char *str);
 
 #endif
--- Cursus42/get_next_line/srcs/get_next_line.c
+++ Cursus42/get_next_line/srcs/get_next_line.c
@@ -6,65 +6,57 @@
 /*   By: mdreesen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 14:45:00 by mdreesen          #+#    #+#             */
-/*   Updated: 2023/04/12 14:45:01 by mdreesen         ###   ########.fr       */
+/*   Updated: 2023/04/13 14:59:01 by mdreesen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 1
 
-char *get_next_line(int fd)
+char	*get_next_line_helper(int ret, char *buffer, char *prev_buffer,
+		char *line)
 {
-    char *line;
-    static char *prev_buffer = NULL;
-    char *buffer;
-    int ret;
-
-    if (prev_buffer == NULL)
-        prev_buffer = malloc(sizeof(char) * BUFFER_SIZE);
-    buffer = malloc(sizeof(char) * BUFFER_SIZE);
-    line = malloc(sizeof(char) * BUFFER_SIZE);
-    line[0] = '\0';
-    if (prev_buffer[0] != '\0')
-        line = ft_strjoin(line, prev_buffer);
-    ret = read(fd, buffer, BUFFER_SIZE);
-    while (ret > 0)
-    {
-        if (ft_strchr(buffer, '\n'))
-        {
-            line = ft_strjoin(line, buffer);
-            free(prev_buffer);
-            prev_buffer = NULL;
-            return (line);
-        }
-        line = ft_strjoin(line, buffer);
-        ret = read(fd, buffer, BUFFER_SIZE);
-    }
-    if (ret == 0 && line[0] == '\0')
-    {
-        free(prev_buffer);
-        prev_buffer = NULL;
-        free(buffer);
-        return (NULL);
-    }
-    free(prev_buffer);
-    prev_buffer = ft_strdup(buffer);
-    free(buffer);
-    return (line);
+	if (ret == 0)
+	{
+		free(buffer);
+		return (line);
+	}
+	free(prev_buffer);
+	prev_buffer = ft_strdup(buffer);
+	free(buffer);
+	return (line);
 }
 
-int main()
+char	*get_next_line(int fd)
 {
-    int fd;
+	char		*line;
+	static char	*prev_buffer;
+	char		*buffer;
+	int			ret;
 
-    fd = open("test.txt", O_RDONLY);
-    printf("%s", get_next_line(fd));
-    printf("%s", get_next_line(fd));
-    printf("%s", get_next_line(fd));
-    return (0);
+	prev_buffer = malloc(sizeof(char) * BUFFER_SIZE);
+	buffer = malloc(sizeof(char) * BUFFER_SIZE);
+	line = malloc(sizeof(char) * BUFFER_SIZE);
+	line[0] = '\0';
+	if (prev_buffer[0] != '\0')
+		line = ft_strjoin(line, prev_buffer);
+	ret = read(fd, buffer, BUFFER_SIZE);
+	while (ret > 0)
+	{
+		if (ft_strchr(buffer, '\n'))
+		{
+			line = ft_strjoin(line, buffer);
+			free(prev_buffer);
+			prev_buffer = NULL;
+			return (line);
+		}
+		line = ft_strjoin(line, buffer);
+		ret = read(fd, buffer, BUFFER_SIZE);
+	}
+	return (get_next_line_helper(ret, buffer, prev_buffer, line));
 }
