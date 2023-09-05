14ab9699322ab4ea01d4c0be3654b8f9eeea3147
Matias Dreesen <mdreesen@student.s19.be>
2023-09-05 15:11:43 +0200

    minitalk norminette/done

--- Cursus42/minitalk/client.c
+++ Cursus42/minitalk/client.c
@@ -6,69 +6,69 @@
 /*   By: matias <matias@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 00:49:52 by matias            #+#    #+#             */
-/*   Updated: 2023/06/27 01:05:57 by matias           ###   ########.fr       */
+/*   Updated: 2023/09/05 15:10:23 by matias           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-    int i;
-    int sign;
-    int res;
+	int	i;
+	int	sign;
+	int	res;
 
-    i = 0;
-    sign = 1;
-    res = 0;
-    while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-        i++;
-    if (str[i] == '-')
-        sign = -1;
-    if (str[i] == '-' || str[i] == '+')
-        i++;
-    while (str[i] && str[i] >= '0' && str[i] <= '9')
-    {
-        res = res * 10 + str[i] - '0';
-        i++;
-    }
-    return (res * sign);
+	i = 0;
+	sign = 1;
+	res = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + str[i] - '0';
+		i++;
+	}
+	return (res * sign);
 }
 
 void	send_char(char c, int pid)
 {
-    int	i;
+	int	i;
 
-    i = 0;
-    while (i < 8)
-    {
-        if (c & 1)
-            kill(pid, SIGUSR1);
-        else
-            kill(pid, SIGUSR2);
-        c >>= 1;
-        usleep(100);
-        i++;
-    }
+	i = 0;
+	while (i < 8)
+	{
+		if (c & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		c >>= 1;
+		usleep(100);
+		i++;
+	}
 }
 
 int	main(int argc, char **argv)
 {
-    int	pid;
-    int	i;
+	int	pid;
+	int	i;
 
-    if (argc != 3)
-    {
-        ft_printf("%sError: wrong number of arguments%s\n", RED, RESET);
-        return (0);
-    }
-    pid = ft_atoi(argv[1]);
-    i = 0;
-    while (argv[2][i])
-    {
-        send_char(argv[2][i], pid);
-        i++;
-    }
-    send_char(argv[2][i], pid);
-    return (0);
+	if (argc != 3)
+	{
+		ft_printf("%sError: wrong number of arguments%s\n", RED, RESET);
+		return (0);
+	}
+	pid = ft_atoi(argv[1]);
+	i = 0;
+	while (argv[2][i])
+	{
+		send_char(argv[2][i], pid);
+		i++;
+	}
+	send_char(argv[2][i], pid);
+	return (0);
 }
--- Cursus42/minitalk/server.c
+++ Cursus42/minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: matias <matias@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 00:49:59 by matias            #+#    #+#             */
-/*   Updated: 2023/06/27 01:29:34 by matias           ###   ########.fr       */
+/*   Updated: 2023/09/05 15:10:51 by matias           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,36 @@
 
 // using signals and not sigaction
 
-void handler(int signum)
+void	handler(int signum)
 {
-    static int c;
-    static int i;
+	static int	c;
+	static int	i;
 
-    if (signum == SIGUSR1)
-        c = c | (1 << i);
-    i++;
-    if (i == 8)
-    {
-        if (c == 0)
-            write(1, "\n", 1);
-        else
-            write(1, &c, 1);
-        c = 0;
-        i = 0;
-    }
+	if (signum == SIGUSR1)
+		c = c | (1 << i);
+	i++;
+	if (i == 8)
+	{
+		if (c == 0)
+			write(1, "\n", 1);
+		else
+			write(1, &c, 1);
+		c = 0;
+		i = 0;
+	}
 }
 
-int main(void)
+int	main(void)
 {
-    int pid;
+	int	pid;
 
-    pid = getpid();
-    ft_printf("%sPID: %d\n%s", RED, pid, RESET);
-    ft_printf("%sWaiting for messages...%s\n", GREEN, RESET);
-    ft_printf("%s|--------------------------------------|%s\n", CYAN, RESET);
-    signal(SIGUSR1, handler);
-    signal(SIGUSR2, handler);
-    while (1)
-        pause();
-    return (0);
+	pid = getpid();
+	ft_printf("%sPID: %d\n%s", RED, pid, RESET);
+	ft_printf("%sWaiting for messages...%s\n", GREEN, RESET);
+	ft_printf("%s|--------------------------------------|%s\n", CYAN, RESET);
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
+	while (1)
+		pause();
+	return (0);
 }
