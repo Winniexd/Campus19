8b38cea5c7873c4d6514621d2145317737714a92
Matias Dreesen <mdreesen@student.s19.be>
2023-11-21 12:21:32 +0100

    WIP

---
+++ Cursus42/push_swap/ps_sort_small.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ps_sort_small.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: matias <matias@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 13:49:05 by marvin            #+#    #+#             */
+/*   Updated: 2023/11/21 12:21:03 by matias           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void ps_sort_two(t_ps *ps)
+{
+    if (ps->stack_a->val > ps->stack_a->next->val)
+        ps_swap(&ps->stack_a);
+}
+
+void ps_sort_three(t_ps *ps)
+{
+    if (ps_is_sorted(ps->stack_a))
+        return;
+    else if (ps->stack_a->val > ps->stack_a->next->val)
+        ps_swap_a(ps);
+    else if (ps->stack_a->val > ps->stack_a->prev->val)
+        ps_reverse_rotate_a(ps);
+    else ps_reverse_rotate_a(ps);
+    ps_sort_three(ps);
+}
+
+void ps_sort_four(t_ps *ps)
+{
+    
+}
+
