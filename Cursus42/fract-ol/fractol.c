0385f3c2dafd923358ce267d7f15585897190d6f
Matias Dreesen <mdreesen@student.s19.be>
2024-01-18 15:49:51 +0100

    fractol error fix

--- Cursus42/fract-ol/fractol.c
+++ Cursus42/fract-ol/fractol.c
@@ -6,7 +6,7 @@
 /*   By: matias <matias@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 14:19:45 by winniexd          #+#    #+#             */
-/*   Updated: 2023/09/05 14:13:35 by matias           ###   ########.fr       */
+/*   Updated: 2024/01/18 15:48:42 by matias           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@ int	main(int argc, char **argv)
 {
 	t_fractol	*fractol;
 
-	fractol = (t_fractol *)ft_calloc(1, sizeof(t_fractol));
-	if (!fractol)
-		return (-1);
+	if (argc != 2 && argc != 4)
+		return (ft_printf("Usage: ./fractol [fractal]\n"));
 	if (argc != 4 && argv[1] && ft_strcmp(argv[1], "julia") == 0)
 		return (ft_printf("Usage: ./fractol julia <float> <float>\n"));
 	else if (argc != 2 && argv[1] && ft_strcmp(argv[1], "julia") == 1)
 		return (ft_printf("Usage: ./fractol [fractal]\n"));
+	fractol = (t_fractol *)ft_calloc(1, sizeof(t_fractol));
+	if (!fractol)
+		return (1);
 	init_mlx(fractol, argv);
 	init_fractol(fractol, argv);
 	mlx_hook(fractol->win, 17, 1L << 2, &destroy_window, fractol);
