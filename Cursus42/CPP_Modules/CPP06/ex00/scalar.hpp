c329cda068451f9bce0b476055ec0db12ea61202
Matias Dreesen <mdreesen@fu-r10-p1.s19.be>
2024-06-24 14:37:54 +0200

    CPP06 looks hard

---
+++ Cursus42/CPP_Modules/CPP06/ex00/scalar.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   scalar.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdreesen <mdreesen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/24 12:52:43 by mdreesen          #+#    #+#             */
+/*   Updated: 2024/06/24 13:35:44 by mdreesen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALAR_HPP
+#define SCALAR_HPP
+#include <iostream>
+
+class ScalarConverter {
+	public:
+		static void convert(char *arg);
+};
+
+#endif
