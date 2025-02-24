3867154831eccb4dab79494fd649259848c25ddd
mdreesen <mdreesen>
2025-02-24 20:45:50 +0000

    CPP09 done

--- Cursus42/CPP_Modules/CPP09/ex02/PmergeMe.hpp
+++ Cursus42/CPP_Modules/CPP09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: winniexd <winniexd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 18:43:54 by winniexd          #+#    #+#             */
-/*   Updated: 2025/02/22 21:08:58 by winniexd         ###   ########.fr       */
+/*   Updated: 2025/02/24 20:34:45 by winniexd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,36 +40,59 @@ class PmergeMe {
         }
 
         template <typename Container>
+        void merge(Container &c, int left, int mid, int right) {
+            Container leftArr(c.begin() + left, c.begin() + mid + 1);
+            Container rightArr(c.begin() + mid + 1, c.begin() + right + 1);
+        
+            long unsigned int i = 0, j = 0, k = left;
+        
+            while (i < leftArr.size() && j < rightArr.size()) {
+                if (leftArr[i] <= rightArr[j]) {
+                    c[k] = leftArr[i];
+                    i++;
+                } else {
+                    c[k] = rightArr[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < leftArr.size()) {
+                c[k] = leftArr[i];
+                i++;
+                k++;
+            }
+            while (j < rightArr.size()) {
+                c[k] = rightArr[j];
+                j++;
+                k++;
+            }
+        }
+        
+
+        template <typename Container>
+        void mergeSort(Container &c, int left, int right) {
+            if (left < right) {
+                int mid = left + (right - left) / 2;
+
+                mergeSort(c, left, mid);
+                mergeSort(c, mid + 1, right);
+
+                merge(c, left, mid, right);
+            }
+        }
+
+        template <typename Container>
         void sort(Container &c) {
             std::cout << "Before: ";
             printElements(c);
             std::cout << std::endl;
             clock_t start = clock();
-            int _Size_half = c.size() / 2;
-
-            Container left(c.begin(), c.begin() + _Size_half);
-            Container right(c.begin() + _Size_half, c.end());
-            Container res;
-            std::sort(left.begin(), left.end());
-            std::sort(right.begin(), right.end());
-            while (!left.empty() && !right.empty()) {
-                if (left.front() == right.front())
-                    throw std::runtime_error("Error: Duplicate value found");
-                if (left.front() < right.front()) {
-                    res.push_back(left.front());
-                    left.erase(left.begin());
-                }
-                else {
-                    res.push_back(right.front());
-                    right.erase(right.begin());
-                }
-            }
-            res.insert(res.end(), left.begin(), left.end());
-            res.insert(res.end(), right.begin(), right.end());
+            
+            mergeSort(c, 0, c.size() - 1);
             
             clock_t end = clock();
             std::cout << "After: ";
-            printElements(res);
+            printElements(c);
             std::cout << std::endl;
             std::string type = (std::string(typeid(c).name()).find("vector") != std::string::npos ? "vector": "deque");
             std::cout << "Time to process a range of " << _Arg_count << " elements with std::" << type << " : ";
--- Cursus42/CPP_Modules/CPP09/ex02/main.cpp
+++ Cursus42/CPP_Modules/CPP09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: winniexd <winniexd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 18:43:58 by winniexd          #+#    #+#             */
-/*   Updated: 2025/02/22 20:37:09 by winniexd         ###   ########.fr       */
+/*   Updated: 2025/02/24 20:44:07 by winniexd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,13 @@ std::vector<int> parseInput(char **argv) {
         int _arg = atoi(argv[i]);
         if (_arg < 0)
             throw std::runtime_error("Error: Non positive int found");
+        std::vector<int>::iterator it = std::find(_vec.begin(), _vec.end(), _arg);
+        if (it != _vec.end())
+            throw std::runtime_error("Error: Duplicate number found");
         _vec.push_back(_arg);
-        } catch(std::exception &) {
-            throw std::runtime_error("Error: Invalid argument");
+        } catch(std::exception &e) {
+            std::cout << e.what() << std::endl;
+            exit(1);
         }
     }
     return _vec;
